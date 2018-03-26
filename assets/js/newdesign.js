13d835d51112c8d7a2b19abf470325eb88309b4e
Eden <ethangooding@gmail.com>
2018-03-26 00:07:11 -0400

    Implement somewhat janky smooth scroll nav

---
+++ assets/js/newdesign.js
@@ -0,0 +1,10 @@
+// import SmoothScroll from "SmoothScroll";
+//
+// var scroll = new SmoothScroll('a[href*="#"]', {
+//   speed: 700,
+//   easing: 'easeInOutCubic'
+// });
+//
+// $(window).scroll(function(){
+//   $(".top").css("opacity", 1 - $(window).scrollTop() / 300);
+// });
